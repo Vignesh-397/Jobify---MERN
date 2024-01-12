e582ad5c72966d09705db79230bc812fd64c2dc3
VIGNESH <nvignesh397@gmail.com>
2024-01-12 21:24:06 +0530

    Added Login and Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="Vignesh" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="Last Name"
+          defaultValue="Poojary"
+        />
+        <FormRow type="text" name="location" defaultValue="Bangalore" />
+        <FormRow type="email" name="email" defaultValue="vignesh@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a momber ?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
